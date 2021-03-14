74243f8a0258aef99aba9aab374ee1c9ce21a989
Jenny Kim <kimj1905@gmail.com>
2021-03-14 18:20:50 -0400

    commit tree fa75bf92400501a7bfa8efffa7a6743e75e174eb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :discount, :quantity, :items, :subtract_last_item
+    attr_reader :title
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.total += quantity * price
+
+        @items.fill(title, @items.size, quantity)
+        
+        # @items.fill(item, @items.size, quantity)
+        # @total += price * quantity
+        # @last_item_price = price * quantity
+        # @total
+        self.subtract_last_item = price * quantity
+    end
+
+    def apply_discount
+        if discount == 0
+            "There is no discount to apply."
+        else
+            self.total *= (100 - discount.to_f)/100
+            "After the discount, the total comes to $#{self.total.to_i}."
+        end
+    end
+
+    def items
+        @items
+        # if @quantity > 1
+        #     @items << @title
+        # else
+        #     @items.new(@quantity, @title)
+        # end
+    end
+
+    def void_last_transaction
+        @total -= self.subtract_last_item
+    end
+    # binding.pry
+end
